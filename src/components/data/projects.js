9b62198f9aa60980a64a3e0274be318667d5b57e
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2023-08-01 19:19:33 +0530

    One more project detail added

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,6 +1,7 @@
 import eCommerceAppImage from "../../assets/e-commerce_app_redux.png";
 import tax_calculator from "../../assets/tax_calculator.png";
 import tic_tac_toe from "../../assets/tic_tac_toe.png";
+import todoApp from "../../assets/todoApp.png";
 export const ProjectData = [
   {
     id: 1,
@@ -13,7 +14,7 @@ export const ProjectData = [
     image: eCommerceAppImage,
   },
   {
-    id: 1,
+    id: 2,
     title: "Tax Calculator",
     about:
       "Utilizing React.js, JavaScript, HTML, and CSS, this project aligns with tax policies, providing an intuitive platform for accurate tax calculations. It offers a seamless user experience while ensuring compliance with updated regulations.",
@@ -23,7 +24,7 @@ export const ProjectData = [
     image: tax_calculator,
   },
   {
-    id: 1,
+    id: 3,
     title: "Tic Tac Toe",
     about:
       "Experience the ultimate Tic Tac Toe game! Using React.js, JavaScript, CSS, Bootstrap, React States, and Events, this immersive web app allows you to challenge friends or play against the computer. Enjoy seamless gameplay on any device with responsive design and intuitive click-based moves. Defeat your opponents in this classic battle of Xs and Os!",
@@ -31,5 +32,23 @@ export const ProjectData = [
     demo: "https://github.com/ThantZinPhyo-Slark/always-illest",
     github: "https://github.com/muddassir1112/ticTacToe",
     image: tic_tac_toe,
+  },
+  {
+    id: 4,
+    title: "Todo App",
+    about:
+      "The Todo App combines Google Firebase, Redux Toolkit, React.js, JavaScript, and Bootstrap for efficient task management. Real-time updates ensure seamless organization, allowing users to create, edit, and prioritize tasks with ease.",
+    tags: [
+      "ReactJS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "Redux Toolkit",
+      "Firebase",
+    ],
+    demo: "https://github.com/ThantZinPhyo-Slark/always-illest",
+    github: "https://github.com/muddassir1112/FirebaseApp_with_toolkit",
+    image: todoApp,
   },
 ];
